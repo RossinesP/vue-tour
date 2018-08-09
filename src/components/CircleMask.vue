32d088ffdeef19eb0ee34d4012da67bb904d2fe9
Pierre Rossinès <pierre.rossines@ecoco2.com>
2018-08-09 17:24:12 +0200

    [feature/overlay] Fix the CircleMask on Webkit

--- src/components/CircleMask.vue
+++ src/components/CircleMask.vue
@@ -2,11 +2,11 @@
   <div class="parent">
     <div class="overlay" ref="overlay" :style="style">
       <svg xmlns="http://www.w3.org/2000/svg">
-        <def>
+        <defs>
           <clipPath id="svgPath">
             <path :d="svgPath" />
           </clipPath>
-        </def>
+        </defs>
       </svg>
     </div>
   </div>
